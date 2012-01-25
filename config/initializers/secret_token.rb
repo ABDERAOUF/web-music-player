31f4161d97cc9a85c7aba882332ae30fc6127271
Jeff Knaggs <mail@jeef3.com>
2012-01-25 14:50:37 +1300

    Initialise new ROR project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebMusicPlayer::Application.config.secret_token = '7697d5e7d9c4f09489a1bc1e72da79fe50f565afe724091df1d7bba2d6feb42b59ba2998b99858c04d74277cc8e3cc4d921e4902a52907b36531e23484042d1f'
