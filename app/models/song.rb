145273d1798ca349d8ce660576c71e91ab194a2f
Jeff Knaggs <mail@jeef3.com>
2012-04-11 21:47:45 +1200

    Update to song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,8 @@
 class Song < ActiveRecord::Base
   attr_accessible :name, :sort_name, :rating, :url
 
+  validates :name, :presence => true
+  validates :rating, :inclusion => { :in => 0..5 }
+
   belongs_to :album
 end
